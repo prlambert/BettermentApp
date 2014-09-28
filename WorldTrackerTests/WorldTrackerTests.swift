b89ba41ca63e081381209b92a9fea6dabad941f7
Paul Roland Lambert <prlambert@users.noreply.github.com>
2014-09-27 17:31:22 -0700

    Initial Commit

---
+++ WorldTrackerTests/WorldTrackerTests.swift
@@ -0,0 +1,36 @@
+//
+//  WorldTrackerTests.swift
+//  WorldTrackerTests
+//
+//  Created by Paul Lambert on 9/27/14.
+//  Copyright (c) 2014 BettermentCo. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class WorldTrackerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
